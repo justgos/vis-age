c330665946ec5a297fe4ff357dc57f6e94e98478
GoS <pup_vas2000@yahoo.com>
2019-12-19 18:53:52 +0200

    Updates

---
+++ precache-manifest.302b4cd97295d7c2da916329b4731ab6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c562a4875a1fcf62a96c966569590bba",
+    "url": "/vis-age/index.html"
+  },
+  {
+    "revision": "cf07cd1ce2e3dd7ed6c6",
+    "url": "/vis-age/static/css/2.1a6d2955.chunk.css"
+  },
+  {
+    "revision": "2a4e9227a331826facd5",
+    "url": "/vis-age/static/css/main.553bcab5.chunk.css"
+  },
+  {
+    "revision": "cf07cd1ce2e3dd7ed6c6",
+    "url": "/vis-age/static/js/2.f3c3d151.chunk.js"
+  },
+  {
+    "revision": "a586e79e4702a4b9b037a012463380c6",
+    "url": "/vis-age/static/js/2.f3c3d151.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2a4e9227a331826facd5",
+    "url": "/vis-age/static/js/main.b9672e6c.chunk.js"
+  },
+  {
+    "revision": "ab20ee6b705e5dafb742",
+    "url": "/vis-age/static/js/runtime-main.bfe59450.js"
+  }
+]);
