123d42a827528d0f027438f0b0d474ffd085f296
GoS <pup_vas2000@yahoo.com>
2019-12-19 19:00:46 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/vis-age/precache-manifest.302b4cd97295d7c2da916329b4731ab6.js"
+  "/vis-age/precache-manifest.fb96e78b1b7215a45ec26cff1cc5298f.js"
 );
 
 self.addEventListener('message', (event) => {
