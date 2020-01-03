a778f05076fe16fa32a70e1484530faa0ec75b8b
GoS <pup_vas2000@yahoo.com>
2020-01-03 15:35:22 +0200

    [CHANGE] Better localisation of the pathway graph nodes

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
 ![](https://github.com/justgos/vis-age/workflows/tests/badge.svg)
 # Visualization of gene expression changes during aging in mouse
 Analysis is done [here](https://github.com/justgos/indagatio-muris-senis)  
-[View online](https://justgos.github.io/vis-age/index.html)  
+[Open UI](https://justgos.github.io/vis-age/index.html)  
+
+```shell
+$ ts-node preprocess.tsx
+```
+
+## Important phenomena that weren't accounted for
+- Impact of post-translational modification (especially phosphorylation) on pathway activity
--- src/App.tsx
+++ src/App.tsx
@@ -60,61 +60,61 @@ function App({
   useEffect(
     () => {
       const loadData = async () => {
-        // Load the expression data
-        const csvData : CsvParseResult = await new Promise(
-          function(complete, error) {
-            Papa.parse(
-              // './data/facs-18m-24m-cell_ontology_class.csv', 
-              './data/merged_augmented.csv', 
-              {
-                delimiter: ',',
-                header: true,
-                download: true,
-                dynamicTyping: true,
-                skipEmptyLines: true,
-                complete, 
-                error
-              }
-            );
-        });
-        updateDataset?.(csvData.data as ExpressionDataRow[]);
-        setFilterDimensions?.(
-          [ 'start_age', 'end_age', 'sex', 'tissue', 'subtissue', 'cell_ontology_class' ]
-        );
-        setFilterValue?.('sex', 'male');
-        [ 'tissue', 'subtissue', 'cell_ontology_class' ].map(d => setFilterValue?.(d, ''));
-        // Text column filter
-        const textColumns = [
-          'tissue', 'subtissue', 'cell_ontology_class', 'gene', 'uniprot_mouse', 'uniprot_daphnia'
-        ];
-        addCustomFilterDimension?.(
-          'text',
-          (row : ExpressionDataRow) => 
-            textColumns.map(d => (row as any)[d]).join('|').toLowerCase(),
-          (filterValue) => {
-            return v => (v as string).includes((filterValue as string).toLowerCase());
-          }
-        );
-        // Daphnia homolog presence filter
-        addCustomFilterDimension?.(
-          'uniprot_daphnia',
-          (row : ExpressionDataRow) => 
-            row.uniprot_daphnia || '',
-          (filterValue) => {
-            if((filterValue as String) === '~') {
-              // Match non-empty values
-              return v => v != null && v !== '';
-            }
-            return v => v === filterValue;
-          }
-        );
+        // // Load the expression data
+        // const csvData : CsvParseResult = await new Promise(
+        //   function(complete, error) {
+        //     Papa.parse(
+        //       // './data/facs-18m-24m-cell_ontology_class.csv', 
+        //       './data/merged_augmented.csv', 
+        //       {
+        //         delimiter: ',',
+        //         header: true,
+        //         download: true,
+        //         dynamicTyping: true,
+        //         skipEmptyLines: true,
+        //         complete, 
+        //         error
+        //       }
+        //     );
+        // });
+        // updateDataset?.(csvData.data as ExpressionDataRow[]);
+        // setFilterDimensions?.(
+        //   [ 'start_age', 'end_age', 'sex', 'tissue', 'subtissue', 'cell_ontology_class' ]
+        // );
+        // setFilterValue?.('sex', 'male');
+        // [ 'tissue', 'subtissue', 'cell_ontology_class' ].map(d => setFilterValue?.(d, ''));
+        // // Text column filter
+        // const textColumns = [
+        //   'tissue', 'subtissue', 'cell_ontology_class', 'gene', 'uniprot_mouse', 'uniprot_daphnia'
+        // ];
+        // addCustomFilterDimension?.(
+        //   'text',
+        //   (row : ExpressionDataRow) => 
+        //     textColumns.map(d => (row as any)[d]).join('|').toLowerCase(),
+        //   (filterValue) => {
+        //     return v => (v as string).includes((filterValue as string).toLowerCase());
+        //   }
+        // );
+        // // Daphnia homolog presence filter
+        // addCustomFilterDimension?.(
+        //   'uniprot_daphnia',
+        //   (row : ExpressionDataRow) => 
+        //     row.uniprot_daphnia || '',
+        //   (filterValue) => {
+        //     if((filterValue as String) === '~') {
+        //       // Match non-empty values
+        //       return v => v != null && v !== '';
+        //     }
+        //     return v => v === filterValue;
+        //   }
+        // );
 
-        // Load pathways
-        let pathways = await axios.get('./data/pathways_preprocessed.json');
-        updatePathways?.(pathways.data as DehydratedPathwayGraph);
-        // Load gene annotations
-        let gene_annotations = await axios.get('./data/gene_annotations.json');
-        updateGeneAnnotations?.(gene_annotations.data as GeneAnnotation[]);
+        // // Load pathways
+        // let pathways = await axios.get('./data/pathways_preprocessed.json');
+        // updatePathways?.(pathways.data as DehydratedPathwayGraph);
+        // // Load gene annotations
+        // let gene_annotations = await axios.get('./data/gene_annotations.json');
+        // updateGeneAnnotations?.(gene_annotations.data as GeneAnnotation[]);
 
         setLoading(false);
       };
